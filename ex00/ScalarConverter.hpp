b80eff036cf7c0207459515a6cceba2fbe96b678
Mihaela <mihaellatocu@yahoo.com>
2025-02-24 13:35:22 +0000

    adding ex00

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#pragma once
+
+class ScalarConverter
+{
+private:
+	/* data */
+public:
+	// ScalarConverter(/* args */);
+	// ~ScalarConverter();
+};
+
+// ScalarConverter::ScalarConverter(/* args */)
+// {
+// }
+
+// ScalarConverter::~ScalarConverter()
+// {
+// }
